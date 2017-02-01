4bdb23f5deec10229c0d3530162b66e4b5040567
Nurahmadie <nurahmadie@gmail.com>
2017-02-01 09:37:04 +0700

    Add initial work for atmega 32u4

---
+++ 32u4/keymap.h
@@ -0,0 +1,21 @@
+#ifndef _HKEYMAP
+#define _H_KEYMAP
+
+#include "keycodes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const char KMAP_BASE_LAYER[][] {
+    {},{},
+    {},{},
+    {},{},
+    {},{},
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
