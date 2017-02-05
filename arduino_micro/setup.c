c2c212d6969e39f82403e6bfa7cf8a04011caf96
Nurahmadie <nurahmadie@gmail.com>
2017-02-06 01:07:32 +0700

    indent with spaces

--- arduino_micro/setup.c
+++ arduino_micro/setup.c
@@ -76,12 +76,12 @@ void EVENT_USB_Device_ConfigurationChanged()
 {
     bool ConfigSuccess = true;
 
-	/* Setup HID Report Endpoints */
-	ConfigSuccess &= Endpoint_ConfigureEndpoint(KEYBOARD_IN_EPADDR, EP_TYPE_INTERRUPT, KEYBOARD_EPSIZE, 1);
-	ConfigSuccess &= Endpoint_ConfigureEndpoint(KEYBOARD_OUT_EPADDR, EP_TYPE_INTERRUPT, KEYBOARD_EPSIZE, 1);
+    /* Setup HID Report Endpoints */
+    ConfigSuccess &= Endpoint_ConfigureEndpoint(KEYBOARD_IN_EPADDR, EP_TYPE_INTERRUPT, KEYBOARD_EPSIZE, 1);
+    ConfigSuccess &= Endpoint_ConfigureEndpoint(KEYBOARD_OUT_EPADDR, EP_TYPE_INTERRUPT, KEYBOARD_EPSIZE, 1);
 
-	/* Turn on Start-of-Frame events for tracking HID report period expiry */
-	USB_Device_EnableSOFEvents();
+    /* Turn on Start-of-Frame events for tracking HID report period expiry */
+    USB_Device_EnableSOFEvents();
 }
 
 void EVENT_USB_Device_StartOfFrame(void)
@@ -93,93 +93,93 @@ void EVENT_USB_Device_StartOfFrame(void)
 void EVENT_USB_Device_ControlRequest(void)
 {
     /* Handle HID Class specific requests */
-	switch (USB_ControlRequest.bRequest)
-	{
-		case HID_REQ_GetReport:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
+    switch (USB_ControlRequest.bRequest)
+    {
+        case HID_REQ_GetReport:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
                 Endpoint_ClearSETUP();
 
                 init_prescan();
                 kbod_matrix_scan();
 
-				/* Write the report data to the control endpoint */
-				Endpoint_Write_Control_Stream_LE(&cReport, sizeof(cReport));
-				Endpoint_ClearIN();
+                /* Write the report data to the control endpoint */
+                Endpoint_Write_Control_Stream_LE(&cReport, sizeof(cReport));
+                Endpoint_ClearIN();
                 Endpoint_ClearStatusStage();
-			}
+            }
 
-			break;
-		case HID_REQ_SetReport:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
+            break;
+        case HID_REQ_SetReport:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
 
-				/* Wait until the LED report has been sent by the host */
-				while (!(Endpoint_IsOUTReceived()))
-				{
-					if (USB_DeviceState == DEVICE_STATE_Unattached)
-					  return;
-				}
+                /* Wait until the LED report has been sent by the host */
+                while (!(Endpoint_IsOUTReceived()))
+                {
+                    if (USB_DeviceState == DEVICE_STATE_Unattached)
+                      return;
+                }
 
-				/* Read in the LED report from the host */
-				uint8_t LEDStatus = Endpoint_Read_8();
+                /* Read in the LED report from the host */
+                uint8_t LEDStatus = Endpoint_Read_8();
 
-				Endpoint_ClearOUT();
-				Endpoint_ClearStatusStage();
+                Endpoint_ClearOUT();
+                Endpoint_ClearStatusStage();
                 // TODO: process report here
-			}
+            }
 
-			break;
-		case HID_REQ_GetProtocol:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
+            break;
+        case HID_REQ_GetProtocol:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
 
-				/* Write the current protocol flag to the host */
-				Endpoint_Write_8(UsingReportProtocol);
+                /* Write the current protocol flag to the host */
+                Endpoint_Write_8(UsingReportProtocol);
 
-				Endpoint_ClearIN();
-				Endpoint_ClearStatusStage();
-			}
+                Endpoint_ClearIN();
+                Endpoint_ClearStatusStage();
+            }
 
-			break;
-		case HID_REQ_SetProtocol:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
-				Endpoint_ClearStatusStage();
+            break;
+        case HID_REQ_SetProtocol:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
+                Endpoint_ClearStatusStage();
 
-				/* Set or clear the flag depending on what the host indicates that the current Protocol should be */
-				UsingReportProtocol = (USB_ControlRequest.wValue != 0);
-			}
+                /* Set or clear the flag depending on what the host indicates that the current Protocol should be */
+                UsingReportProtocol = (USB_ControlRequest.wValue != 0);
+            }
 
-			break;
-		case HID_REQ_SetIdle:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
-				Endpoint_ClearStatusStage();
+            break;
+        case HID_REQ_SetIdle:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
+                Endpoint_ClearStatusStage();
 
-				/* Get idle period in MSB, idleCount must be multiplied by 4 to get number of milliseconds */
-				idleCount = ((USB_ControlRequest.wValue & 0xFF00) >> 6);
-			}
+                /* Get idle period in MSB, idleCount must be multiplied by 4 to get number of milliseconds */
+                idleCount = ((USB_ControlRequest.wValue & 0xFF00) >> 6);
+            }
 
-			break;
-		case HID_REQ_GetIdle:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
+            break;
+        case HID_REQ_GetIdle:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
 
-				/* Write the current idle duration to the host, must be divided by 4 before sent to host */
-				Endpoint_Write_8(idleCount >> 2);
+                /* Write the current idle duration to the host, must be divided by 4 before sent to host */
+                Endpoint_Write_8(idleCount >> 2);
 
-				Endpoint_ClearIN();
-				Endpoint_ClearStatusStage();
-			}
+                Endpoint_ClearIN();
+                Endpoint_ClearStatusStage();
+            }
 
-			break;
-	}
+            break;
+    }
 }
 
 
@@ -197,38 +197,38 @@ void idle_USBTask()
 void send_report()
 {
     static KBOD_Report_t prevReport;
-	bool                 sendReport = false;
+    bool                 sendReport = false;
 
-	/* Check if the idle period is set and has elapsed */
-	if (IdleCount && (!(IdleMSRemaining)))
-	{
-		/* Reset the idle time remaining counter */
-		IdleMSRemaining = IdleCount;
+    /* Check if the idle period is set and has elapsed */
+    if (IdleCount && (!(IdleMSRemaining)))
+    {
+        /* Reset the idle time remaining counter */
+        IdleMSRemaining = IdleCount;
 
-		/* Idle period is set and has elapsed, must send a report to the host */
-		SendReport = true;
-	}
-	else
-	{
-		/* Check to see if the report data has changed - if so a report MUST be sent */
-		sendReport = (memcmp(&prevReport, &cReport, sizeof(KBOD_Report_t)) != 0);
-	}
+        /* Idle period is set and has elapsed, must send a report to the host */
+        SendReport = true;
+    }
+    else
+    {
+        /* Check to see if the report data has changed - if so a report MUST be sent */
+        sendReport = (memcmp(&prevReport, &cReport, sizeof(KBOD_Report_t)) != 0);
+    }
 
-	/* Select the Keyboard Report Endpoint */
-	Endpoint_SelectEndpoint(KEYBOARD_IN_EPADDR);
+    /* Select the Keyboard Report Endpoint */
+    Endpoint_SelectEndpoint(KEYBOARD_IN_EPADDR);
 
-	/* Check if Keyboard Endpoint Ready for Read/Write and if we should send a new report */
-	if (Endpoint_IsReadWriteAllowed() && sendReport)
-	{
-		/* Save the current report data for later comparison to check for changes */
-		prevReport = cReport;
+    /* Check if Keyboard Endpoint Ready for Read/Write and if we should send a new report */
+    if (Endpoint_IsReadWriteAllowed() && sendReport)
+    {
+        /* Save the current report data for later comparison to check for changes */
+        prevReport = cReport;
 
-		/* Write Keyboard Report Data */
-		Endpoint_Write_Stream_LE(&cReport, sizeof(cReport), NULL);
+        /* Write Keyboard Report Data */
+        Endpoint_Write_Stream_LE(&cReport, sizeof(cReport), NULL);
 
-		/* Finalize the stream transfer to send the last packet */
-		Endpoint_ClearIN();
-	}
+        /* Finalize the stream transfer to send the last packet */
+        Endpoint_ClearIN();
+    }
 }
 
 void receive_report()
